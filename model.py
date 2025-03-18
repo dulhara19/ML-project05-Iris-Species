d94117da2a2886a08bba2c4bc827dfe35744ed04
dulhara <wkldulhara@gmail.com>
2025-03-18 23:43:39 +0530

    added libs

---
+++ model.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
