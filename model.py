8b24df1e2a21d5c50e0f3060315e8ea75715773a
dulhara <wkldulhara@gmail.com>
2025-03-18 23:48:22 +0530

    prediction

--- model.py
+++ model.py
@@ -24,3 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Train Random Forest Model
 rf = RandomForestClassifier(n_estimators=100, random_state=42)
 rf.fit(X_train, y_train)
+
+
+# Predictions
+y_pred = rf.predict(X_test)
