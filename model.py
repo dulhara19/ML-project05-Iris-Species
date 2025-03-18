cb37221ca48e4d4e3faf6463746eb07c88247b1d
dulhara <wkldulhara@gmail.com>
2025-03-19 00:58:22 +0530

    model created syuccesfully

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.datasets import load_iris
@@ -6,25 +7,31 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
+from sklearn.preprocessing import LabelEncoder
+
+#setting random seed
+np.random.seed(42)
+
 # Load dataset from CSV
-df = pd.read_csv("iris.csv")  # Update with your file path if needed
+df = pd.read_csv("Iris.csv") # Update with your file path if needed
+print(df.head())
 
-# Encode species column (if it's categorical)
+# Encode species column (As its categorical)
 le = LabelEncoder()
-df['species'] = le.fit_transform(df['species'])
-
+df['Species'] = le.fit_transform(df['Species'])
+print(df.head())
 
 # Split into features (X) and target (y)
-X = df.drop(columns=['species'])
-y = df['species']
-
-# Train-test split
+X = df.drop(columns=['Species'])
+# print(X)
+y = df['Species']
+# print(y)
+# # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train Random Forest Model
 rf = RandomForestClassifier(n_estimators=100, random_state=42)
 rf.fit(X_train, y_train)
-
 
 # Predictions
 y_pred = rf.predict(X_test)
@@ -40,4 +47,12 @@ sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, cmap="Blues", fmt="d",
 plt.xlabel("Predicted")
 plt.ylabel("Actual")
 plt.title("Confusion Matrix")
+plt.show()
+
+# Feature Importance
+plt.figure(figsize=(8, 4))
+sns.barplot(x=rf.feature_importances_, y=X.columns)
+plt.xlabel("Feature Importance Score")
+plt.ylabel("Features")
+plt.title("Feature Importance in Random Forest")
 plt.show()
